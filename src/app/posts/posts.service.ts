f9b333cb6a0faf368c6aef33238546150cd30e42
George Codat Tudoran <office@solcreation.ro>
2018-07-18 18:40:16 +0300

    Adding observables to the service

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,16 +1,24 @@
-import { Post } from './post.model';
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
-@Injectable({providedIn: 'root'})
+import { Post } from './post.model';
+
+@Injectable({providedIn: 'root'}) /* makes service available on root level withot adding it within providers in the module component   */
 export class PostsService {
   private posts: Post[] = [];
+  private postsUpdated = new Subject<Post[]>();
 
   getPosts() {
     return [...this.posts];
   }
 
+  getPostUpdateListener() {
+    return this.postsUpdated.asObservable();
+  }
+
   addPost(title: string, content: string) {
     const post: Post = {title: title, content: content};
     this.posts.push(post);
+    this.postsUpdated.next([...this.posts]);
   }
 }
