c0bb71272e792b373fed2d16a1fbb2736dac697d
George Codat Tudoran <office@solcreation.ro>
2018-08-01 23:46:15 +0300

    Fetching initial posts from the server

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,28 @@
 const express = require('express')
 const app = express()
 
-app.use((req, res, next) => {
-  console.log('First middleware');
-  next();
+
+app.use('/api/posts', (req, res, next) => {
+  const posts = [
+    {
+      id: '1',
+      title: 'First post',
+      content: 'This is coming from the server'
+    },
+    {
+      id: '2',
+      title: 'Second post',
+      content: 'This is coming from the server 2!'
+    },
+
+  ];
+
+  res.status(200).json({
+    message: 'Posts fetched succesfully!',
+    posts: posts
+  });
 });
 
-app.use((req, res, next) => {
-    res.send("Message from express");
-});
+
 
 module.exports = app;
