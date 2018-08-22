e237fd884026c046d258277e1778462888d270fb
George Codat Tudoran <office@solcreation.ro>
2018-08-22 19:37:05 +0300

    Conencting to Amazon Cloud MongoDB servers

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,21 @@
 const express = require('express')
-const app = express();
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const constants = require('./constants');
 
 const Post = require('./models/post');
+
+const app = express();
+
+let mongoDBPass = constants.appPasswords.MONGO_DB_PASSWOPRD;
+mongoose.connect('mongodb+srv://georgevc15:'+mongoDBPass+'@cluster0-96lzh.mongodb.net/test?retryWrites=true')
+      .then(()=> {
+        console.log('Conected to database');
+      })
+      .catch(() => {
+        console.log('Connection failed');
+      });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
