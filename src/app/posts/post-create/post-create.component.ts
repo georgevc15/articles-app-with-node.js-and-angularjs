f11338a43f6235dd1fd9d6e6b7fba99a61fef5b4
George Codat Tudoran <office@solcreation.ro>
2018-09-20 22:29:06 +0300

    Edit post component

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 
 import { PostsService } from './../posts.service';
+import { Post } from '../post.model';
 
 @Component({
   selector: 'app-post-create',
@@ -10,11 +11,27 @@ import { PostsService } from './../posts.service';
   styleUrls: ['./post-create.component.css']
 })
 
-export class PostCreateComponent {
+export class PostCreateComponent implements OnInit {
     enteredTitle = '';
     enteredContent = '';
+    private mode = 'create';
+    private postId: string;
+    private post: Post;
 
-    constructor(public postService: PostsService ) {}
+    constructor(public postService: PostsService, public route: ActivatedRoute ) {}
+
+ngOnInit() {
+  this.route.paramMap.subscribe((paramMap: ParamMap) => {
+      if (paramMap.has('postId')) {
+        this.mode = 'edit';
+        this.postId = paramMap.get('postId');
+        this.post = this.postService.getPost(this.postId);
+      } else {
+        this.mode = 'create';
+        this.postId = null;
+      }
+  });
+}
 
     onAddPost(form: NgForm) {
       // console.dir(postInput);
