09210ff667959ff2196b70571d0a8e58b29cbfe0
George Codat Tudoran <office@solcreation.ro>
2018-10-19 15:12:11 +0300

    Adding Image Controls to Store the Image

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -30,7 +30,8 @@ ngOnInit() {
     }),
     'content': new FormControl(null, {
       validators: [Validators.required]
-    })
+    }),
+    'image': new FormControl(null, {validators: [Validators.required]})
   });
   this.route.paramMap.subscribe((paramMap: ParamMap) => {
       if (paramMap.has('postId')) {
@@ -56,6 +57,13 @@ ngOnInit() {
   });
 }
 
+onImagePicked(event: Event) {
+  const file = (event.target as HTMLInputElement).files[0];
+  this.form.patchValue({image: file});
+  this.form.get('image').updateValueAndValidity();
+  console.log(file);
+  console.log(this.form);
+}
     onSavePost() {
       // console.dir(postInput);
       if (this.form.invalid) {
