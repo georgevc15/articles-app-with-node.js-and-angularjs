e268bd83d0d702dc954a807484cca3136cd1e21e
George Codat Tudoran <office@solcreation.ro>
2018-11-09 20:36:56 +0200

    Backend logic for pagination

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -78,7 +78,17 @@ router.put(
 );
 
 router.get("", (req, res, next) => {
-  Post.find().then(documents => {
+  console.log(req.query);
+  const pageSize = +req.query.pagesize;
+  const currentPage = +req.query.page;
+  const postQuery = Post.find();
+  console.log(req.query);
+  if(pageSize && currentPage) {
+    postQuery
+      .skip(pageSize * (currentPage - 1))
+      .limit(pageSize);
+  }
+  postQuery.find().then(documents => {
     res.status(200).json({
       message: "Posts fetched successfully!",
       posts: documents
