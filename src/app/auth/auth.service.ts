44067e618a1dfdaa2a68c8db15a5e3c49bd9f8dd
George Codat Tudoran <office@solcreation.ro>
2018-12-14 22:25:11 +0200

    Saving the Token in the Local Storage - methods

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -47,9 +47,23 @@ export class AuthService {
           }, expiresInDuration * 1000);
           this.isAuthenticated = true;
           this.authStatusListener.next(true);
+          const now = new Date();
+          const expirationDate = new Date(now.getTime() + expiresInDuration * 1000);
+          this.saveAuthData(token, expirationDate);
           this.router.navigate(['/']);
         }
       });
+  }
+
+  autoAuthUser() {
+    const authInformation = this.getAuthData();
+    const now = new Date();
+    const isInFuture = authInformation.expirationDate > now;
+    if (isInFuture) {
+      this.token = authInformation.token;
+      this.isAuthenticated = true;
+      this.authStatusListener.next(true);
+    }
   }
 
   logout() {
@@ -57,7 +71,29 @@ export class AuthService {
     this.isAuthenticated = false;
     this.authStatusListener.next(false);
     clearTimeout(this.tokenTimer);
+    this.clearAuthData();
     this.router.navigate(['/']);
   }
 
+    private saveAuthData(token: string, expirationDate: Date) {
+      localStorage.setItem('token', token);
+      localStorage.setItem('expiration', expirationDate.toISOString());
+    }
+
+    private clearAuthData() {
+      localStorage.removeItem('token');
+      localStorage.removeItem('expiration');
+    }
+
+    private getAuthData() {
+       const token = localStorage.getItem('token');
+       const expirationDate = localStorage.getItem('expiration');
+       if (token || expirationDate) {
+          return;
+       }
+       return {
+         token: token,
+         expirationDate: new Date(expirationDate)
+       };
+    }
 }
