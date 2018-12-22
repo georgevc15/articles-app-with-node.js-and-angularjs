d6d4a55c3914bf9b90f69148a013776bcad4c9f6
George Codat Tudoran <office@solcreation.ro>
2018-12-22 21:10:39 +0200

    Adding error messages on the server

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -53,6 +53,11 @@ router.post(
           id: createdPost._id
         }
       });
+    })
+    .catch(error => {
+      res.status(500).json({
+          message: "Post creation failed!"
+      });
     });
   }
 );
@@ -81,6 +86,11 @@ router.put(
       } else {
         res.status(401).json({ message: "Not authorized!" });
       }
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: "Couldn't update post!"
+      });
     });
   }
 );
@@ -104,6 +114,11 @@ router.get("", (req, res, next) => {
         posts: fetchedPosts,
         maxPosts: count
       });
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: "Fetching posts failed!"
+      });
     });
 });
 
@@ -114,6 +129,11 @@ router.get("/:id", (req, res, next) => {
     } else {
       res.status(404).json({ message: "Post not found!" });
     }
+  })
+  .catch(error => {
+    res.status(500).json({
+      message: "Fetching posts failed!"
+    });
   });
 });
 
@@ -127,6 +147,11 @@ router.delete("/:id", checkAuth, (req, res, next) => {
     } else {
       res.status(401).json({ message: "Not authorized!" });
     }
+  })
+  .catch(error => {
+    res.status(500).json({
+      message: "Deleting post failed!"
+    });
   });
 });
 
